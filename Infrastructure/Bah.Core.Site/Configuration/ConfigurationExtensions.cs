3512391a22ad3c2079927811c5b1e61133dd76b9
Scott Stafford <scott.stafford@gmail.com>
2015-11-17 11:12:55 -0500

    Built a variable-replacing configuration module.

---
+++ Infrastructure/Bah.Core.Site/Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Bah.Core.Site.Configuration;
+using Microsoft.Framework.Configuration;
+using System;
+using System.IO;
+
+namespace Bah.Core.Site.Configuration
+{
+    public static class ConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddJsonFile(this IConfigurationBuilder configurationBuilder, string path, object variables, bool optional)
+        {
+            if (configurationBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(configurationBuilder));
+            }
+
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentException("Invalid path", nameof(path));
+            }
+
+            var fullPath = Path.Combine(configurationBuilder.GetBasePath(), path);
+
+            if (!optional && !File.Exists(fullPath))
+            {
+                throw new FileNotFoundException("File not found.", fullPath);
+            }
+
+            configurationBuilder.Add(new JsonConfigurationProviderWithSubstitution(fullPath, optional: optional, variables: variables));
+            return configurationBuilder;
+        }
+
+        public static IConfigurationBuilder AddJsonFile(this IConfigurationBuilder configurationBuilder, string path, object variables)
+        {
+            return configurationBuilder.AddJsonFile(path, variables, false);
+        }
+    }
+}
---
+++ Infrastructure/Bah.Core.Site/Configuration/JsonConfigurationProviderWithSubstitution.cs
@@ -0,0 +1,36 @@
+﻿using Bah.Core.Site.Utils;
+using Microsoft.Framework.Configuration.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bah.Core.Site.Configuration
+{
+    public class JsonConfigurationProviderWithSubstitution : JsonConfigurationProvider
+    {
+        public JsonConfigurationProviderWithSubstitution(string path, bool optional, object variables)
+            : base(path, optional)
+        {
+            this.Variables = variables;
+        }
+
+        private object Variables;
+        public override void Load()
+        {
+            base.Load();
+
+            if (this.Variables == null)
+                return;
+
+            var keys = new List<string>(this.Data.Keys);
+            foreach (var key in keys)
+            {
+                var value = this.Data[key];
+                if (value == null) continue;
+                var newValue = FormattableObject.Format(Variables, value, null);
+                this.Data[key] = newValue;
+            }
+        }
+    }
+}
